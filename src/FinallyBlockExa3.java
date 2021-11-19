99c096fab0119cf2819f4d938ae760e825820874
Yash Gupta <yash8917gupta@gmail.com>
2021-11-19 23:20:03 +0530

    When an exception occurs and is handled by the catch block

---
+++ src/FinallyBlockExa3.java
@@ -0,0 +1,13 @@
+public class FinallyBlockExa3 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(11/0);
+        }
+        catch (ArithmeticException obj){
+            System.out.println("Exception is raised and conroll is in catch block");
+        }
+        finally {
+            System.out.println("Finally block is always executed");
+        }
+    }
+}
