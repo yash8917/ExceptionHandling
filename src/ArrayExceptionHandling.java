4118cb8a6bf3def1dbb3aab97706efb4d9b43635
Yash Gupta <yash8917gupta@gmail.com>
2021-11-20 21:15:29 +0530

    Initial commit

---
+++ src/ArrayExceptionHandling.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+//Write a program that takes as input the size of the array and the
+// elements in the array. The program then asks the user to enter
+// a particular index and prints the element at that index. Index
+// starts from zero.
+//        This program may generate Array Index Out Of Bounds Exception
+//        or NumberFormatException .  Use exception handling mechanisms
+//
+//        to handle this exception.
+//
+//        Sample Input and Output 1:
+//        Enter the number of elements in the array
+//        2
+//        Enter the elements in the array
+//        50
+//        80
+//        Enter the index of the array element you want to access
+//        1
+//        The array element at index 1 = 80
+//        The array element successfully accessed
+//
+//
+//        Sample Input and Output 2:
+//        Enter the number of elements in the array
+//        2
+//        Enter the elements in the array
+//        50
+//        80
+//        Enter the index of the array element you want to access
+//        9
+//        java.lang.ArrayIndexOutOfBoundsException
+//
+//
+//        Sample Input and Output 3:
+//        Enter the number of elements in the array
+//        2
+//        Enter the elements in the array
+//        30
+//        j
+//        java.lang.NumberFormatException
+public class ArrayExceptionHandling {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter the number of elements in the array:");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+         int a[]= new int[n];
+        try{
+        for (int i = 0; i < n; i++) {
+            a[i]=sc.nextInt();
+        }
+            System.out.println("Enter the number what you want to access:");
+        int index=sc.nextInt();
+            System.out.println("The Array index an "+index+":-"+a[index]);
+            System.out.println("The array element successfully accessed");
+        }
+        catch (ArrayIndexOutOfBoundsException obj){
+            System.out.println(obj);
+        }
+        catch (NumberFormatException obj1){
+            System.out.println(obj1);
+        }
+
+}}
